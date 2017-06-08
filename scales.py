64000e4da28f0f2e7bdeba156baaecdb4fb737f0
Benjamin-Kozuch <Benjamin.kozuch@nyu.edu>
2017-06-08 16:06:00 -0400

    make snake grow when it eats

--- scales.py
+++ scales.py
@@ -1,23 +1,62 @@
 class Scale(object):
-	def __init__(self, x_position, y_position, size):
+    def __init__(self, x_position, y_position, size=10):
         self.x_position = x_position
         self.y_position = y_position
         self.size = size   
 
-
-
 class Snake(object):
-	def __init__(self):
-		head = Scale(50,50,10) #(x,y,size)
-		self.scales = []
-		self.scales.append(head)
+    def __init__(self):
+        head = Scale(50,50,10) #(x,y,size)
+        self.scales = []
+        self.scales.append(head)
+        self.x_direction = 1
+        self.y_direction = 0 
+        self.speed = 10
 
-	def move(self, direction):
-		pass
+    def move(self): #move each scale ahead one.
+        
+        #Move every scale in the tail into the position in front of it.
+        for index, scale in reversed(list(enumerate(self.scales))):
+            if index >= 1:
+                self.scales[index].x_position = self.scales[index-1].x_position
+                self.scales[index].y_position = self.scales[index-1].y_position
 
-	def eat(self):
-		pass
+        #Move head of snake into next position
+        self.scales[0].x_position += self.speed * self.x_direction
+        self.scales[0].y_position += self.speed * self.y_direction
 
-	def grow(self):
-		pass
+
+        #(3)(2)(1)
+        #(-)(-)(0)
+
+   
+        #(-)(-)(-)(-)(-)
+        #   (-)(-)(-)(-)(-)
+        #(-)(-)(-)(-)(-)(-)   
+    def can_eat(self, food): # i.e. collision
+        if self.scales[0].x_position == food.x_position and self.scales[0].y_position == food.y_position:
+        	return True
+        else:
+        	return False
+
+    def eat_and_grow(self,food): # add a scale
+    
+        #save tail end scale in temp var
+        temp_scale_x = self.scales[len(self.scales)-1].x_position
+        temp_scale_y = self.scales[len(self.scales)-1].y_position
+        temp_scale_size = self.scales[len(self.scales)-1].size
+
+        #move the snake
+        self.move()
+
+        #append the food to the temp location
+        self.scales.append(Scale(temp_scale_x, temp_scale_y, temp_scale_size))
+        #self.scales.append(temp_scale)
+
+        for scale in self.scales:
+            print (str(scale.x_position) + ", " + str(scale.y_position))
+
+
+
+
 
--- snake.py
+++ snake.py
@@ -1,24 +1,82 @@
 import pygame
-pygame.init()
+import scales
+import random
+import math
 
+pygame.init()
 screen_width = 600
 screen_height = 400
 screen = pygame.display.set_mode((screen_width, screen_height))  
-
 clock = pygame.time.Clock()
 
+def create_food():
+    x_random = math.floor(random.random() * screen_width) 
+    y_random = math.floor(random.random() * screen_height)
+
+    x_random -= (x_random % 10)
+    y_random -= (y_random % 10)
+
+    food = scales.Scale(x_random, y_random)
+    return food
+
+#Every snake starts off with a head and then grows a tail
+snake = scales.Snake()
 
 #Game Loop
 done = False
+food_is_eaten = False
+food = create_food()
+count = 0
+
 while not done:
+    
+    count+=1
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
 
-    screen.fill((255, 255, 255)) 
+    #Change Snake Direction
+    if pygame.key.get_pressed()[pygame.K_UP]:
+        snake.x_direction = 0
+        snake.y_direction = -1
+    if pygame.key.get_pressed()[pygame.K_DOWN]:
+        snake.x_direction = 0
+        snake.y_direction = 1        
+    if pygame.key.get_pressed()[pygame.K_LEFT]:
+        snake.x_direction = -1
+        snake.y_direction = 0        
+    if pygame.key.get_pressed()[pygame.K_RIGHT]: 
+        snake.x_direction = 1
+        snake.y_direction = 0       
 
-    pygame.draw.rect(screen, (0, 128, 255), pygame.Rect(50, 50, 10, 10))
+    #Create New Food if the old one has not been eaten
+    if food_is_eaten:
+        food = create_food()
+        food_is_eaten = False
+
+    #Move the Snake
+    if count % 5 == 0: snake.move()
+    
+    if snake.can_eat(food): #check for collision
+        snake.eat_and_grow(food)
+        food_is_eaten = True
+        print (len(snake.scales))
+    
+    #Clear the previous screen
+    screen.fill((0, 0, 0)) 
+    
+    #Draw Snake
+    for scale in snake.scales:
+        pygame.draw.rect(screen, (50,205,50), pygame.Rect(scale.x_position, scale.y_position, scale.size, scale.size))
+
+    #Draw Food
+    pygame.draw.rect(screen, (50,205,50), pygame.Rect(food.x_position, food.y_position, food.size, food.size))
+    
+
 
     pygame.display.flip()
-    
-    clock.tick(120)
+    clock.tick(60)
+
+
+
+
